b241ade5ed6b80c94ddc2f2e67db583f3dd81865
Leo Champion <leo.champion@protonmail.com>
2019-02-11 22:57:06 +0100

    Parse items bonus
    - todo: handle 'resistance au combat' for shield bonus

--- dofus_retrocraft_scrapper/spiders/items.py
+++ dofus_retrocraft_scrapper/spiders/items.py
@@ -34,10 +34,63 @@ class ItemsSpider(scrapy.Spider):
         s = s.replace('<td class="ing">', '').replace('</td>', '')
         recipe_items_match = [ItemsSpider.re_recipe.match(e) for e in s.split('<br>')[:-1]]
         return [{'item': e.group('item_name'), 'count': int(e.group('item_count'))} for e in recipe_items_match]
+    
 
     @staticmethod
-    def parse_stats(div):
-        pass
+    def parse_values(match):
+        return [int(match.group('value_min')), int(match.group('value_max'))] if match.group('multi_values') else int(match.group('value'))
+
+    @staticmethod
+    def parse_bonus_classical(match):
+        res = {}
+        res['type'] = match.group('bonus_type')
+        if match.group('percentage'):
+            res['type'] = "% " + res['type']
+        if match.group('element'):
+            res['type'] += " " + match.group('element') 
+        res['values'] = ItemsSpider.parse_values(match)
+        return res
+
+    @staticmethod
+    def parse_bonus_exception(match, type):
+        res = {}
+        res['type'] = type
+        res['values'] = ItemsSpider.parse_values(match)
+        return res
+
+    rs_number = r'[-+]?[0-9]+'
+    rs_value = rf'((?P<multi_values><.*>(?P<value_min>{rs_number})<.*> à <.*>(?P<value_max>{rs_number})<.*>)|(?P<single_value><.*>(?P<value>{rs_number})<.*>))'
+    re_classical_bonus = re.compile(rf'^(?P<classical_bonus>(?P<bonus_type>.+) : {rs_value}(?P<percentage>%)?( \((?P<element>[a-z]+)\))?)$')
+    re_dommage = re.compile(rf'^(?P<dommages>{rs_value} de dommages)$')
+    re_perc_dommage = re.compile(rf'^(?P<perc_dommage>Augmente les dommages de {rs_value}%)$')
+    re_perc_dommage_trap = re.compile(rf'^(?P<perc_dommage_trap>{rs_value}% de dommages au pièges)$')
+    @staticmethod
+    def parse_bonus(div):
+        s = div.css('.effet').extract_first()
+        s = s.replace('<td class="effet">', '').replace('</td>', '')
+        items = s.split('<br>')[:-1]
+        res = []
+        for item in items:
+            bonus = {}
+            match_classical_bonus = ItemsSpider.re_classical_bonus.match(item)
+            if match_classical_bonus is not None:
+                ItemsSpider.parse_bonus_classical(match_classical_bonus)
+                break
+            match_dommage = ItemsSpider.re_dommage.match(item)
+            if match_dommage is not None:
+                ItemsSpider.parse_bonus_exception(match_dommage, 'Dommage')
+                break
+            match_perc_dommage = ItemsSpider.re_perc_dommage.match(item)
+            if match_perc_dommage is not None:
+                ItemsSpider.parse_bonus_exception(match_dommage, '\% de dommage')
+                break
+            if match_perc_dommage is not None:
+                ItemsSpider.parse_bonus_exception(match_dommage, '\% de dommage au piège')
+                break
+            bonus['type'] = re.sub(r'<[^<>]+>', '', item)
+            print(bonus['type'])
+            res.append(bonus)
+        return res
 
     @staticmethod
     def parse_requirements(div):
@@ -51,8 +104,9 @@ class ItemsSpider(scrapy.Spider):
                 'name': ItemsSpider.parse_name(item_component),
                 'level': ItemsSpider.parse_level(item_component),
                 'recipe': ItemsSpider.parse_recipe(item_component),
-                'stats': ItemsSpider.parse_stats(item_component.css('.effet')),
+                'bonus': ItemsSpider.parse_bonus(item_component),
                 'requirments': ItemsSpider.parse_requirements(item_component.css('.itions'))
             }
             items.append(item)
-        return { 'items': items }
+        item_type = response.css('.donjon::text').get()
+        return { item_type: items }
