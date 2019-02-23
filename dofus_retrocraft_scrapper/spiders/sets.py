b106d9d4d4ab1006801b91ce9dce9d5b2e1528d2
Leo Champion <leo.champion@protonmail.com>
2019-02-24 00:11:23 +0100

    Handle sets bonus in classical case

--- dofus_retrocraft_scrapper/spiders/sets.py
+++ dofus_retrocraft_scrapper/spiders/sets.py
@@ -10,6 +10,18 @@ class SetsSpider(scrapy.Spider):
     start_urls = [
         f'{base_url}/accpano.html',
     ]
+
+    re_bonus = r'(\+)?(?P<value>[0-9]+) (?P<type>.*)'
+    @staticmethod
+    def extract_bonus(bonus_string):
+        match = re.match(SetsSpider.re_bonus, bonus_string)
+        if match is None:
+            return {'error': bonus_string}
+        return {
+            'type': match.group('type'),
+            'value': match.group('value')
+        }
+
     def parse_set(self, response):
         bonus_by_items_count_str = response.css('.plies::text').getall()
         items = response.css('.nom::text').getall()
@@ -18,9 +30,8 @@ class SetsSpider(scrapy.Spider):
             if bonus == 'Aucun bonus':
                 b = []
             else:
-                b = bonus.split(' / ')
+                b = [SetsSpider.extract_bonus(bs) for bs in bonus.split(' / ')]
             bonus_by_items_count.append(b)
-
         return {
             'items': items,
             'bonus': bonus_by_items_count
