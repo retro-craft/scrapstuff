38e44bce30bdb51acb2328e41530331e3b2cbccb
Leo Champion <leo.champion@protonmail.com>
2019-03-02 12:38:43 +0100

    Parse all bonus sets

--- dofus_retrocraft_scrapper/spiders/sets.py
+++ dofus_retrocraft_scrapper/spiders/sets.py
@@ -11,16 +11,32 @@ class SetsSpider(scrapy.Spider):
         f'{base_url}/accpano.html',
     ]
 
-    re_bonus = r'(\+)?(?P<value>[0-9]+) (?P<type>.*)'
+
+
+    re_bonus_perc = re.compile(r'(\+)?(?P<value>[0-9]+)(?P<type>% .*)')
+    re_bonus = re.compile(r'(\+)?(?P<value>[0-9]+) (?P<type>.*)')
+    re_bonus_strange_space = re.compile(r'(\+)?( )?(?P<value>[0-9]+)( )?(?P<type>(PA)|(PM)|(Portée)|(CC)|(Dommages))')
+    re_bonus_reduction = re.compile(r'(?P<type>Réduction ((physique)|(magique))) de (?P<value>[0-9]+)')
+    re_bonus_return = re.compile(r'(?P<type>Renvoie) (?P<value>[0-9]+) dommages')
+    lre_bonus = [
+        re_bonus,
+        re_bonus_perc,
+        re_bonus_strange_space,
+        re_bonus_reduction,
+        re_bonus_return
+    ]
     @staticmethod
     def extract_bonus(bonus_string):
-        match = re.match(SetsSpider.re_bonus, bonus_string)
-        if match is None:
-            return {'error': bonus_string}
-        return {
-            'type': match.group('type'),
-            'value': match.group('value')
-        }
+        bonus = {}
+        for re_b in SetsSpider.lre_bonus:
+            match = re_b.match(bonus_string)
+            if match is not None:
+                bonus['type'] = match.group('type')
+                bonus['value'] = match.group('value')
+                break
+        if bonus == {}:
+            bonus['error'] = bonus_string
+        return bonus
 
     def parse_set(self, response):
         bonus_by_items_count_str = response.css('.plies::text').getall()
